4a61b23492bcb98b25a2bef8d5ac3cd53e341a5e
Rishi Raj <rishiraj824@gmail.com>
2016-09-15 18:05:37 +0530

    npm-manager-init

---
+++ index.js
@@ -0,0 +1,22 @@
+var picture = require('cat-picture')
+var src = picture.src
+picture.remove()
+
+var image = require('lightning-image-poly')
+var viz = new image ('#visualization' , null, [src] , {hullAlgorithm : 'convex'})
+var remote = require('electron').remote
+var fs = require('fs')
+
+function save(){
+	remote.getCurrentWindow().webContents.printToPDF({
+		portrait:true
+	}, function(err, data){
+		fs.writeFile('annotation.pdf', data, function(err){
+			if(err) alert('error generating pdf!' + err.message)
+			else alert('pdf saved!')
+		})
+	})
+}
+window.addEventListener('keydown',function(e){
+	if (e.keyCode == 80) save()	
+})
