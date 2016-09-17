e3c31b07f008e0912954c84a6265c57f25faa059
Rishi Raj <rishiraj824@gmail.com>
2016-09-17 21:59:30 +0530

    added photon though not using it for now

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var electron  = require('electron');
+const remote = require('electron').remote;
+
 
 electron.app.on('ready' , function(){
-	var mainWindow = new electron.BrowserWindow({width:600 , height :800})
+	var mainWindow = new electron.BrowserWindow({useContentSize:true, title:"NPM Package Manager", autoHideMenuBar:true, thickFrame:true})
+	mainWindow.isResizable(false);
+	mainWindow.webContents.openDevTools();
 	mainWindow.loadURL('file://' + __dirname + '/index.html')	
 })
